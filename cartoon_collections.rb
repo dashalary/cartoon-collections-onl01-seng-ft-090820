e086169638ec50122de71d66e8b6f11698e0c5af
Dasha Lary <dk2800@nyu.edu>
2020-09-10 22:56:18 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -18,6 +18,7 @@ def find_the_cheese(stringarray)
   cheese_types = ["cheddar", "gouda", "camembert"]
   cheesearr = []
   # if stringarray.include?("cheddar") || stringarray.include?("gouda") || stringarray.include?("camembert")
+  stringarray.find 
     return 
     
   else 
