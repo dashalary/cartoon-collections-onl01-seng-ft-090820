dfd085fd16b55d4dfe607bda04b80a9dab4c4ada
Dasha Lary <dk2800@nyu.edu>
2020-09-10 21:47:47 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,5 +1,5 @@
 def roll_call_dwarves(dwarfarray)
-  dwarfarray.each_with_index { |dwarf, index| 
+  dwarfarray.each_with_index { |dwarf, index| puts "#{dwarf}" }
 end
 
 def summon_captain_planet# code an argument here
